7100ff3942bd4ad8b9ca5ada68c9d8c6709f2c1f
danilfs009@yandex.ru <62214743+danilfs@users.noreply.github.com>
2021-12-27 09:27:46 +0300

    made another layout and id for buttons

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -1,27 +1,18 @@
 package com.example.mycalculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        result = findViewById(R.id.result);
-    }
-
-    public void onNumberClick(View view) {
-
-        Button button = (Button) view;
-        result.append(button.getText());
     }
 }
+
