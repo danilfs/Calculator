21b8ae3d08567da6751d379e21cd8cd9ef4eb228
danilfs009@yandex.ru <62214743+danilfs@users.noreply.github.com>
2021-12-27 09:57:56 +0300

    numbers input and clear button

---
+++ app/src/main/java/com/example/mycalculator/CalculatorModel.java
@@ -0,0 +1,110 @@
+package com.example.mycalculator;
+
+public class CalculatorModel {
+
+    private int firstArg;
+    private int secondArg;
+
+    private StringBuilder inputStr = new StringBuilder();
+
+
+
+    private State state;
+
+    private enum State {
+        firstArgInput,
+        operationSelected,
+        secondArgInput,
+        resultShow
+    }
+
+    public CalculatorModel() {
+        state = State.firstArgInput;
+    }
+
+    public void onNumPressed(int buttonId) {
+
+        if (state == State.resultShow) {
+            state = State.firstArgInput;
+            inputStr.setLength(0);
+        }
+
+        if (state == State.operationSelected) {
+            state = State.secondArgInput;
+            inputStr.setLength(0);
+        }
+
+        if (inputStr.length() < 9) {
+            switch (buttonId) {
+                case R.id.zero:
+                    if (inputStr.length() != 0) {
+                        inputStr.append("0");
+                    }
+                    break;
+                case R.id.one:
+                    inputStr.append("1");
+                    break;
+                case R.id.two:
+                    inputStr.append("2");
+                    break;
+                case R.id.three:
+                    inputStr.append("3");
+                    break;
+                case R.id.four:
+                    inputStr.append("4");
+                    break;
+                case R.id.five:
+                    inputStr.append("5");
+                    break;
+                case R.id.six:
+                    inputStr.append("6");
+                    break;
+                case R.id.seven:
+                    inputStr.append("7");
+                    break;
+                case R.id.eight:
+                    inputStr.append("8");
+                    break;
+                case R.id.nine:
+                    inputStr.append("9");
+                    break;
+            }
+        }
+
+    }
+
+
+
+    public String getText() {
+        StringBuilder str = new StringBuilder();
+        switch (state) {
+            default:
+                return inputStr.toString();
+            case operationSelected:
+                return str.append(firstArg).append(' ')
+
+                        .toString();
+            case secondArgInput:
+                return str.append(firstArg).append(' ')
+
+                        .append(' ')
+                        .append(inputStr)
+                        .toString();
+            case resultShow:
+                return str.append(firstArg).append(' ')
+
+                        .append(' ')
+                        .append(secondArg)
+                        .append(" = ")
+                        .append(inputStr.toString())
+                        .toString();
+        }
+    }
+
+
+
+    public void reset() {
+        state = State.firstArgInput;
+        inputStr.setLength(0);
+    }
+}
